6223faeae6ed4268450a99f290af98cf6b03ccc5
jcreus <joan.creus.c@gmail.com>
2015-02-21 20:23:15 +0100

    msg

--- pendulum.js
+++ pendulum.js
@@ -1,44 +1,9 @@
 function Vector(e,t,n){this.x=e||0;this.y=t||0;this.z=n||0}Vector.prototype={negative:function(){return new Vector(-this.x,-this.y,-this.z)},add:function(e){if(e instanceof Vector)return new Vector(this.x+e.x,this.y+e.y,this.z+e.z);else return new Vector(this.x+e,this.y+e,this.z+e)},subtract:function(e){if(e instanceof Vector)return new Vector(this.x-e.x,this.y-e.y,this.z-e.z);else return new Vector(this.x-e,this.y-e,this.z-e)},multiply:function(e){if(e instanceof Vector)return new Vector(this.x*e.x,this.y*e.y,this.z*e.z);else return new Vector(this.x*e,this.y*e,this.z*e)},divide:function(e){if(e instanceof Vector)return new Vector(this.x/e.x,this.y/e.y,this.z/e.z);else return new Vector(this.x/e,this.y/e,this.z/e)},equals:function(e){return this.x==e.x&&this.y==e.y&&this.z==e.z},dot:function(e){return this.x*e.x+this.y*e.y+this.z*e.z},cross:function(e){return new Vector(this.y*e.z-this.z*e.y,this.z*e.x-this.x*e.z,this.x*e.y-this.y*e.x)},length:function(){return Math.sqrt(this.dot(this))},unit:function(){return this.divide(this.length())},min:function(){return Math.min(Math.min(this.x,this.y),this.z)},max:function(){return Math.max(Math.max(this.x,this.y),this.z)},toAngles:function(){return{theta:Math.atan2(this.z,this.x),phi:Math.asin(this.y/this.length())}},angleTo:function(e){return Math.acos(this.dot(e)/(this.length()*e.length()))},toArray:function(e){return[this.x,this.y,this.z].slice(0,e||3)},clone:function(){return new Vector(this.x,this.y,this.z)},init:function(e,t,n){this.x=e;this.y=t;this.z=n;return this}};Vector.negative=function(e,t){t.x=-e.x;t.y=-e.y;t.z=-e.z;return t};Vector.add=function(e,t,n){if(t instanceof Vector){n.x=e.x+t.x;n.y=e.y+t.y;n.z=e.z+t.z}else{n.x=e.x+t;n.y=e.y+t;n.z=e.z+t}return n};Vector.subtract=function(e,t,n){if(t instanceof Vector){n.x=e.x-t.x;n.y=e.y-t.y;n.z=e.z-t.z}else{n.x=e.x-t;n.y=e.y-t;n.z=e.z-t}return n};Vector.multiply=function(e,t,n){if(t instanceof Vector){n.x=e.x*t.x;n.y=e.y*t.y;n.z=e.z*t.z}else{n.x=e.x*t;n.y=e.y*t;n.z=e.z*t}return n};Vector.divide=function(e,t,n){if(t instanceof Vector){n.x=e.x/t.x;n.y=e.y/t.y;n.z=e.z/t.z}else{n.x=e.x/t;n.y=e.y/t;n.z=e.z/t}return n};Vector.cross=function(e,t,n){n.x=e.y*t.z-e.z*t.y;n.y=e.z*t.x-e.x*t.z;n.z=e.x*t.y-e.y*t.x;return n};Vector.unit=function(e,t){var n=e.length();t.x=e.x/n;t.y=e.y/n;t.z=e.z/n;return t};Vector.fromAngles=function(e,t){return new Vector(Math.cos(e)*Math.cos(t),Math.sin(t),Math.sin(e)*Math.cos(t))};Vector.randomDirection=function(){return Vector.fromAngles(Math.random()*Math.PI*2,Math.asin(Math.random()*2-1))};Vector.min=function(e,t){return new Vector(Math.min(e.x,t.x),Math.min(e.y,t.y),Math.min(e.z,t.z))};Vector.max=function(e,t){return new Vector(Math.max(e.x,t.x),Math.max(e.y,t.y),Math.max(e.z,t.z))};Vector.lerp=function(e,t,n){return t.subtract(e).multiply(n).add(e)};Vector.fromArray=function(e){return new Vector(e[0],e[1],e[2])};Vector.angleBetween=function(e,t){return e.angleTo(t)}
-
+inter=-1
 document.getElementById("hi").onclick = function () {
-var a = "";
-var AU = 149597871000;
-
-var sun = new Body("S", new Vector(4.412960100445353E+05, -1.056006919786789E+05, -2.093236097217392E+04),
-			new Vector(6.573363288776926E-03, 9.401663922864669E-03, -1.648805743878167E-04),
-			1.989E30)
-
-var mercury = new Body("M", new Vector(-2.442026052766887E+07, 4.091849619827504E+07, 5.612032065956395E+06),
-			new Vector(-5.146591592410186E+01, -2.336536564028074E+01, 2.812296976418871E+00),
-			3.301E23)
-
-var venus = new Body("V", new Vector(1.087146158106242E+08, 6.564805355636189E+06,-6.177851138661536E+06),
-			new Vector(-2.290040961427047E+00, 3.480550877156311E+01, 6.092762688705751E-01).multiply(-1),
-			4.867E24);
-
-var earth = new Body("E", new Vector(-9.134782488005780E+07, 1.151424995446428E+08, -2.415242112524211E+04),
-			new Vector(-2.376962619918467E+01, -1.866728576284719E+01, 1.283436655342306E-03),
-			5.98E24)
-
-var mars = new Body("A", new Vector( 2.082313367041399E+08, 2.209663381533407E+07, -4.655797836948094E+06
-),
-			new Vector( -1.635488517824704E+00, 2.617336693185755E+01, 5.883578119690935E-01),
-			6.416E23)
-
-var jupiter = new Body("J", new Vector( -5.799997767497101E+08, 5.463692495987649E+08, 1.069776396212671E+07),
-			new Vector( -9.113150818834210E+00,-8.894739485136334E+00, 2.409389972078974E-01),
-			1.898E27)
-
-
-var best = new Body("B", new Vector( -6.663262270974609E+07, 2.466817235110883E+08, 1.752217483768138E+07),
-			new Vector( -2.417084355596645E+01, 1.042120058412594E+01, 5.798911610049436E+00),
-			1)
-
-var bodies = [sun, mercury, venus, earth, best, mars, jupiter];
-
-var a = acc(bodies);
-
-
+clearInterval(inter);
+ggbApplet.reset();
+ggbApplet.refreshViews();
 function print(v) {
 	return "("+v.x/AU+","+v.y/AU+","+v.z/AU+")";
 }
@@ -52,22 +17,6 @@ function Body(nom, pos, vel, mass) {
 
 	this.move = function () {
 		ggbApplet.evalCommand(this.nom+"="+print(this.pos));
-		ggbApplet.evalCommand(this.nom+this.nom+"="+print(this.pos));
-		if (a != "") {
-			var vec = a(this.pos, this.vel, this.mass).unit();
-			//vec = vec.divide(vec.length());
-			//vec = this.pos.add(vec);//.add(this.vel.unit());
-			//console.log(print(vec));
-			//ggbApplet.evalCommand("a"+this.nom+"=Vector["+this.nom+","+this.nom+"+"+print(a(this.pos,this.vel,this.mass).unit().multiply(AU).divide(2))+"]");
-			ggbApplet.setLabelVisible("a"+this.nom,false);
-			// acceleració
-			//ggbApplet.evalCommand("a"+this.nom+"=Vector["+this.nom+","+this.nom+"+"+print(a(this.pos,this.vel,this.mass).unit().multiply(AU).divide(2))+"]");
-
-			// moment angular
-			ggbApplet.evalCommand("a"+this.nom+"=Vector["+this.nom+","+this.nom+"+"+print(this.pos.cross(this.vel).unit().multiply(AU))+"]");
-
-		//ggbApplet.evalCommand(this.nom+this.nom+this.nom+"="+this.nom+"+a"+this.nom);}
-		}
 		ggbApplet.evalCommand(this.nom+this.nom+"="+print(this.pos));
 		//ggbApplet.evalCommand("a"+this.nom+"="+print(a(this.pos,this.vel,this.mass).unit()));
 	}
@@ -105,7 +54,7 @@ function rk4(body) {
   return [xf, vf];
 }
 
-var dt = 1.5*86400;
+var dt = 0.005;
 
 function euler(body) {
      body.vel = body.vel.add(a(body.pos,body.vel,body.mass).multiply(dt));
@@ -125,11 +74,54 @@ function acc(bodies) {
 	}
 	return f;
 }
-setInterval(function () {
-	for (var j=0; j<bodies.length; j++) {
-		euler(bodies[j]);
-		bodies[j].move();
-	}
+
+var theta1 = -1.5*Math.PI/4;//-Math.PI/4;
+var theta2 = Math.PI/2;
+
+ggbApplet.evalCommand("O=(0,0)")
+ggbApplet.evalCommand("A=(0,0)")
+ggbApplet.evalCommand("B=(0,0)")
+ggbApplet.evalCommand("a=Segment[O,A]")
+ggbApplet.evalCommand("b=Segment[A,B]")
+ggbApplet.setLabelVisible("a",false);
+ggbApplet.setLabelVisible("b",false);
+function update () {
+	ggbApplet.evalCommand("A=("+l1*Math.sin(theta1)+","+(-l1*Math.cos(theta1))+")")
+	ggbApplet.evalCommand("B=("+(l1*Math.sin(theta1)+l2*Math.sin(theta2))+","+(-l1*Math.cos(theta1)-l2*Math.cos(theta2))+")")
+	ggbApplet.evalCommand("AA=A");
+	ggbApplet.evalCommand("BB=B");
+	ggbApplet.setTrace("AA", true);
+	ggbApplet.setTrace("BB", true);
+	ggbApplet.setColor("AA",0,0,0);
+	ggbApplet.setColor("BB",0,0,0);
+	ggbApplet.setPointSize("AA", 2);
+	ggbApplet.setPointSize("BB", 2);
+	ggbApplet.setPointSize("A", 4);
+	ggbApplet.setPointSize("B", 4);
+	ggbApplet.setLabelVisible("A",false);
+	ggbApplet.setLabelVisible("AA",false);
+	ggbApplet.setLabelVisible("B",false);
+	ggbApplet.setLabelVisible("BB",false);
+}
+var m1=eval(document.getElementById("m1").value);
+var m2=eval(document.getElementById("m2").value);
+var l1=eval(document.getElementById("l1").value);
+var l2=eval(document.getElementById("l2").value);
+var theta1=eval(document.getElementById("a1").value);
+var theta2=eval(document.getElementById("a2").value);
+
+var g=9.8;
+var dtheta2 = 0;
+var dtheta1 = 0;
+inter=setInterval(function () {
+	mu      =  1+m1/m2;
+	d2theta1  =  (g*(Math.sin(theta2)*Math.cos(theta1-theta2)-mu*Math.sin(theta1))-(l2*dtheta2*dtheta2+l1*dtheta1*dtheta1*Math.cos(theta1-theta2))*Math.sin(theta1-theta2))/(l1*(mu-Math.cos(theta1-theta2)*Math.cos(theta1-theta2)));
+	d2theta2  =  (mu*g*(Math.sin(theta1)*Math.cos(theta1-theta2)-Math.sin(theta2))+(mu*l1*dtheta1*dtheta1+l2*dtheta2*dtheta2*Math.cos(theta1-theta2))*Math.sin(theta1-theta2))/(l2*(mu-Math.cos(theta1-theta2)*Math.cos(theta1-theta2)));
+	dtheta1   += d2theta1*dt;
+	dtheta2   += d2theta2*dt;
+	theta1    += dtheta1*dt;		
+	theta2    += dtheta2*dt;
+	update();
 },10);
 }
 
